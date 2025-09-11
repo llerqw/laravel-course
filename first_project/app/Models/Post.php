13389e6d0724733a8e699fc6150f7147e72bebf0
Valeri <werlq@yandex.ru>
2025-09-11 19:01:57 +0300

    Урок 5. Модели

--- first_project/app/Models/Post.php
+++ first_project/app/Models/Post.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Post extends Model // Все св-ва и методы наследуются из класса Model
 {
     use HasFactory;
+//    Урок 5. Модели.
 }
