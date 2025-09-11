47bcce7c098f1b08732ff614c832b858c544665f
Valeri <werlq@yandex.ru>
2025-09-11 10:26:58 +0300

    Урок 2. Роуты

--- first_project/routes/web.php
+++ first_project/routes/web.php
@@ -14,5 +14,44 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'aaaaaaaaaaaaaaaa';
+});
+
+Route::get('/my_page', function () {
+    return 'this is my page';
+});
+
+
+//8 роутов
+
+Route::get('/my_name', function (){
+    return 'my name is Lera';
+});
+
+Route::get('/my_age', function (){
+    return 'im 18';
+});
+
+Route::get('/my_city', function (){
+    return 'my is city';
+});
+
+Route::get('/my_hobby', function (){
+    return 'my hobby is reading';
+});
+
+Route::get('/my_pet', function (){
+    return 'my pet is cat';
+});
+
+Route::get('/my_fav_food', function (){
+    return 'my favorite food is pizza';
+});
+
+Route::get('/my_fav_music', function (){
+    return 'my favorite music is sza';
+});
+
+Route::get('/my_fav_book', function (){
+    return 'my favorite book is Haruki Murakami Norwegian Forest';
 });
