8d4c6b5b4039f537416fd312428ef64cf3d1eaa5
llerqw <lodyginavaleria337@gmail.com>
2025-09-22 11:00:23 +0300

    Урок 23. CRUD через интерфейс - модифицируем интерфейс часть 4

--- first_project/resources/views/post/create.blade.php
+++ first_project/resources/views/post/create.blade.php
@@ -13,33 +13,52 @@
             @csrf
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" id="title" name="title">
+                <input value="{{ old('title') }}" type="text" class="form-control" id="title" name="title">
+                @error('title')
+                <p class="mt-2 text-danger">{{ $message }}</p>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="content" class="form-label">Content</label>
-                <textarea class="form-control" id="content" name="content"></textarea>
+                <textarea class="form-control" id="content" name="content">{{ old('content') }}</textarea>
+                @error('content')
+                <p class="mt-2 text-danger">{{ $message }}</p>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="image" class="form-label">Image</label>
-                <input type="text" class="form-control" id="image" name="image">
+                <input {{ old('image') }} type="text" class="form-control" id="image" name="image">
+                @error('image')
+                <p class="mt-2 text-danger">{{ $message }}</p>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label for="category" class="form-label">Category</label>
                 <select class="form-select" id="category" name="category_id">
                     @foreach($categories as $category)
-                        <option value="{{$category->id}}">{{$category->title}}</option>
+                        <option
+                            {{ old('category_id') == $categories->id ? ' selected' : ''}}
+                            value="{{$category->id}}">{{$category->title}}</option>
                     @endforeach
                 </select>
+                @error('category_id')
+                <p class="mt-2 text-danger">{{ $message }}</p>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label for="tags" class="form-label">Tags</label>
                 <select class="form-select" multiple id="tags" name="tags[]">
                     @foreach($tags as $tag)
-                        <option value="{{$tag->id}}">{{$tag->title}}</option>
+                        <option
+                            {{ old('tags') == $categories->id ? ' selected' : ''}}
+                            value="{{$tag->id}}">{{$tag->title}}</option>
                     @endforeach
                 </select>
+                @error('tags')
+                <p class="mt-2 text-danger">{{ $message }}</p>
+                @enderror
             </div>
 
 
