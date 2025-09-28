14b6933b9661bd2241d39e25921d6ab19933d082
llerqw <lodyginavaleria337@gmail.com>
2025-09-28 17:13:26 +0300

    Урок 33. Класс Middleware

--- first_project/routes/web.php
+++ first_project/routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'HomeController@index');
+Route::get('/', 'HomeController@index')->name('home');
 
 Route::group(['namespace' => 'Post'], function () {
     Route::get('/posts', 'IndexController')->name('post.index');
@@ -25,7 +25,7 @@ Route::group(['namespace' => 'Post'], function () {
     Route::delete('posts/{post}', 'DestroyController')->name('post.delete');
 });
 
-Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => 'admin'], function () {
     Route::group(['namespace' => 'Post'], function () {
         Route::get('/post', 'IndexController')->name('admin.post.index');
 
