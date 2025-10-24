0907be38688c0ae88cb9a6fb2a00e266ece96490
llerqw <lodyginavaleria337@gmail.com>
2025-10-24 14:27:20 +0300

    Excel

---
+++ first_project/app/Console/Commands/ImportExelCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Components\ImportDataClient;
+use App\Imports\PostsImport;
+use App\Models\Post;
+use Illuminate\Console\Command;
+use Maatwebsite\Excel\Facades\Excel;
+
+class ImportExelCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:excel';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get data from exel';
+
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        ini_set('memory_limit', '-1');
+        Excel::import(new PostsImport(), public_path('excel/posts.xlsx'));
+        dd('finished');
+    }
+}
